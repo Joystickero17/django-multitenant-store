a266d677013650acc73bef81400161668c8e33f8
Augusto Carrillo <64995946+Joystickero17@users.noreply.github.com>
2023-01-20 22:02:46 -0400

    test

--- core/controllers/receipt_controller.py
+++ core/controllers/receipt_controller.py
@@ -10,8 +10,6 @@ def generate_receipt(order: Order):
         "base_url": settings.BASE_URL
     }
     html = render_to_string("receipt.html", context)
-    # with open("prueba.html", "w") as f:
-    #     f.write(html)
     byte_data = HTML(string=html, base_url=settings.BASE_URL).write_pdf()
 
     return byte_data
