446a71c80a3b19f446575bf0dba66082089d7ac8
Augusto Carrillo <64995946+Joystickero17@users.noreply.github.com>
2023-02-05 18:20:43 -0400

    fixing bug in profile img url socket

--- core/serializers/chat_serializer.py
+++ core/serializers/chat_serializer.py
@@ -25,16 +25,19 @@ class ChatMessageSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         instance = super().create(validated_data)
         to_user = validated_data.get('to_user')
+        has_profile_img = 
         consumer_data={
         "type":"chat.message",
         "message":"Ha llegado un nuevo mensaje",
         "content":f"{validated_data.get('content')}",
         "from_user":f"{instance.from_user.pk}",
         "from_user_email":f"{instance.from_user.email}",
-        "from_user_img":f"{instance.from_user.profile_img.url}",
+        
         "to_user":f"{to_user.pk}",
         "created_at": f"{instance.created_at.isoformat()}"
         }
+        if hasattr(instance.from_user.profile_img, "url"):
+            consumer_data.update({"from_user_img":f"{instance.from_user.profile_img.url}",})
         async_to_sync(channel_layer.group_send)(f'chat{to_user.pk}', consumer_data)
         return instance
 
